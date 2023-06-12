8fe81d1d70df47976aa563853cbb2e77ff73ecad
DanGrutsyuk <Dan_Gritsyuk>
2023-06-12 11:42:16 +0300

    Rename decorator

---
+++ Decorators/CalcLogDecorator.java
@@ -0,0 +1,22 @@
+package Decorators;
+
+import Controllers.LoggerController;
+import Exercises.Homework1.Exercise3;
+
+public class CalcLogDecorator extends Exercise3 {
+
+    public CalcLogDecorator(Exercise3 exercise) {
+        super(exercise.GetDescription());
+    }
+
+    private LoggerController _logger = new LoggerController(Exercise3.class.getName(), false);
+
+    @Override
+    public boolean Solution() {
+        Boolean result = super.Solution();
+        String message = super.strResult;
+        _logger.Log(message);
+        return result;
+    }
+
+}
